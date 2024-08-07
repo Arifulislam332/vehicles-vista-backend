7aeb893641021b612712d44913afad85694ee241
Arifulislam332 <ayatullaharif332@gmail.com>
2024-08-07 19:38:51 +0600

    feat: jwt check

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { createCurrentUser } from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth.middleware";
 
 const router: Router = express.Router();
 
 // CREATE CURRENT USER
-router.post("/", createCurrentUser);
+router.post("/", jwtCheck, createCurrentUser);
 
 export default router;
